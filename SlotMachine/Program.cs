1bc3860a2b2487d264a26218e982a3612aec7ea0
James M. Allen <james.m.allen@gmail.com>
2016-06-23 14:06:12 -0400

    SlotMachine class with initial properties

--- SlotMachine/Program.cs
+++ SlotMachine/Program.cs
@@ -8,6 +8,15 @@ namespace SlotMachine
 {
     class Program
     {
+        /*
+         * Welcome message
+         * Directions
+         * Choose a machine
+         * Place a bet
+         * Pull lever
+         * Randomly rolls around and gives you 
+         * Machine tells you if you won or not
+         */
         static void Main(string[] args)
         {
         }
