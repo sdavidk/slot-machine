25d1b137be4d502cd3fc5aa803f260de8a319153
James M. Allen <james.m.allen@gmail.com>
2016-06-23 14:06:29 -0400

    --amend

---
+++ SlotMachine/SlotMachine.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlotMachine
+{
+    class SlotMachine
+    {
+        /*
+         * Classes We Make:
+         * SlotMachine
+         * Properties:
+         * - NumberOfSlots
+         * - IconsPerSlot
+         * - MinimumBet
+         * - MaximumBet
+         * - CurrentBet
+         * Methods:
+         * - PlaceBet(int numberOfPennies)
+         * - PullLever()
+         * - DisplayResults()
+         * - GetPayout()
+         */
+
+        public int NumberOfSlots { get; set; }
+        public int IconsPerSlot { get; set; }
+        public int MinimumBet { get; set; }
+        public int MaximumBet { get; set; }
+
+        private int _currentBet;
+        public int CurrentBet
+        {
+            get
+            {
+                return _currentBet;
+            }
+            set
+            {
+                _currentBet = value;
+                if (_currentBet < MinimumBet)
+                {
+                    _currentBet = MinimumBet;
+                }
+                if (_currentBet > MaximumBet)
+                {
+                    _currentBet = MaximumBet;
+                }
+            }
+        }
+
+    }
+}
